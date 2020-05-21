75d61a62c52bc2043436d8fbc013060a9dbf9252
Rakka Alhazimi <rakkakeren@gmail.com>
2020-05-22 00:00:07 +0700

    Part 2 Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World !"
+    user = {"username":"Rakka"}
+    posts = [
+        {
+            "author": {"username":"Johny"},
+            "body":"Beautiful day in Africa"
+        },
+        {
+            "author": {"username":"Susan"},
+            "body":"The Anime Movie was so cool!"
+        },
+        {
+            "author": {"username": "Raizo"},
+            "body": "The strongest ninja has arrive!"
+        }
+
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
