277381009469f98c126f28015f3f830ff34f6a67
Rakka Alhazimi <rakkakeren@gmail.com>
2020-05-22 12:56:22 +0700

    Part3 Web Forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+print(__name__)
 
 from app import routes # circular import
---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()]) # validator, check that the field is not submited empty
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
