185f637470db45756ff2dbd71f6ab35f071bcb04
Rakka Alhazimi <rakkakeren@gmail.com>
2020-05-23 04:26:59 +0700

    Part 5 User Login

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()]) # validator, check that the field is not submited empty
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username): # validate_<field-name>
+        user = User.query.filter_by(username=username.data).first() # first(), query method to get one result
+        if user is not None:
+            raise ValidationError("Please use different username.")
+        # The message included as the argument in the exception
+        # will be the message that will be displayed next to the field for the user to see.
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Please use different email address.")
 
 
 # Create form flow: install flask-wtf
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required # If you decorate a view with this, it will ensure that the current user is
+                # logged in and authenticated before calling the actual view.
 def index():
-    user = {"username":"Rakka"}
     posts = [
         {
             "author": {"username":"Johny"},
@@ -21,18 +25,53 @@ def index():
         }
 
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
     if form.validate_on_submit(): # If the client request a POST method.
+        user = User.query.filter_by(username=form.username.data).first()
+        # if the login not success
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+
+        # if the login success
         flash("Login requested for user {}, remember_me={}".format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
+
     return render_template("login.html", title="Sign In", form=form)
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 # routes flow: add decorator @app.route(url) and
 #              create a func that return html string or render template
