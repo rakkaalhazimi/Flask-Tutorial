4a3a02ab6ca918b1e3f1d02a6048e3c0da60b9df
Rakka Alhazimi <rakkakeren@gmail.com>
2020-05-23 16:11:13 +0700

    Part 6 Profile Page and Avatars

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -29,5 +29,10 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError("Please use different email address.")
 
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
 
 # Create form flow: install flask-wtf
