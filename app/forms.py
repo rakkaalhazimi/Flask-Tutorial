dc32223c1f0c4b9343bbcfcf443dd62d195638ab
Rakka Alhazimi <rakkakeren@gmail.com>
2020-05-29 22:10:05 +0700

    Part 9 Pagination

--- app/forms.py
+++ app/forms.py
@@ -50,5 +50,10 @@ class EmptyForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+class PostForm(FlaskForm):
+    post = TextAreaField("Say something", validators=[
+        DataRequired(), Length(min=1, max=140)
+    ])
+    submit = SubmitField("Submit")
 
 # Create form flow: install flask-wtf
--- config.py
+++ config.py
@@ -3,9 +3,11 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess" # get value from environ or use fallback value
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or\
         "sqlite:///" + os.path.join(base_dir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Signal app whenever the database is about to change
+    POST_PER_PAGE = 3
 
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
@@ -13,5 +15,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["rakkakeren@gmail.com"]
+
 
 # Config flow: var = os.environ(key) or fallback value
